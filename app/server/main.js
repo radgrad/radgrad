c875c40dfe3b9e3f3c7af3831cf09359c21bce2c
Philip Johnson <johnson@hawaii.edu>
2016-10-17 16:12:51 -1000

    Minor tweaks

--- app/server/main.js
+++ app/server/main.js
@@ -1,1 +1,16 @@
+import '/imports/api/base';
+import '/imports/api/career';
+import '/imports/api/course';
+import '/imports/api/degree';
+import '/imports/api/feedback';
+import '/imports/api/ice';
+import '/imports/api/interest';
+import '/imports/api/opportunity';
+import '/imports/api/role';
+import '/imports/api/semester';
+import '/imports/api/slug';
+import '/imports/api/star';
+import '/imports/api/user';
+import '/imports/api/work';
+
 import '/imports/startup/server';
