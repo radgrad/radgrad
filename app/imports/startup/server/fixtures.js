0519d46f7e7e767285a78b6c239981212bb7b200
Cam Moore <cammymoore07@gmail.com>
2016-10-23 11:32:07 -1000

    Defining Courses.

--- app/imports/startup/server/fixtures.js
+++ app/imports/startup/server/fixtures.js
@@ -1,6 +1,8 @@
 import { Meteor } from 'meteor/meteor';
 
 import { Semesters } from '../../api/semester/SemesterCollection.js';
+import { Courses } from '../../api/course/CourseCollection.js';
+import { courseDefinitions } from './icsdata/CourseDefinitions.js';
 
 // if the database is empty on server start, create some sample data.
 Meteor.startup(() => {
@@ -14,5 +16,8 @@ Meteor.startup(() => {
     Semesters.define({ term: Semesters.FALL, year: 2017 });
     Semesters.define({ term: Semesters.SPRING, year: 2018 });
     Semesters.define({ term: Semesters.SUMMER, year: 2018 });
+  }
+  if (Courses.find().count() === 0) {
+    courseDefinitions.map((definition) => Courses.define(definition));
   }
 });
