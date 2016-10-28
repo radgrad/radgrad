8fa7ca5e4c749435724315982321fa2270f42d77
Philip Johnson <johnson@hawaii.edu>
2016-10-27 14:28:32 -1000

    finished rough mockup

--- app/imports/ui/components/index.js
+++ app/imports/ui/components/index.js
@@ -1,3 +1,12 @@
 import './advisor-tab-student.html';
 import './advisor-tab-student.js';
 
+import './verification-event.html';
+import './verification-event.js';
+
+import './verification-requests-completed.html';
+import './verification-requests-completed.js';
+
+import './verification-requests-pending.html';
+import './verification-requests-pending.js';
+
--- app/imports/ui/components/verification-requests-completed.js
+++ app/imports/ui/components/verification-requests-completed.js
@@ -1,22 +1,19 @@
 import { Template } from 'meteor/templating';
 
-Template.Advisor_Home_Page.onCreated(function appBodyOnCreated() {
+Template.Verification_Requests_Completed.onCreated(function appBodyOnCreated() {
   // placeholder: typically you will put global subscriptions here if you remove the autopublish package.
 });
 
-Template.Advisor_Home_Page.helpers({
+Template.Verification_Requests_Completed.helpers({
   // placeholder: if you display dynamic data in your layout, you will put your template helpers here.
 });
 
-Template.Advisor_Home_Page.events({
+Template.Verification_Requests_Completed.events({
  // placeholder: if you add a form to this top-level layout, handle the associated events here.
 });
 
-Template.Advisor_Home_Page.onRendered(function enableDropDown() {
+Template.Verification_Requests_Completed.onRendered(function enableDropDown() {
   this.$('.dropdown').dropdown({
     // action: 'select',
-  });
-  this.$('.tabular.menu .item').tab({
-    alwaysRefresh: true,
   });
 });
