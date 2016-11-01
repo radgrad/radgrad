6946f6dac93a0bd8459e13a8e20c649a3164d0aa
Cam Moore <cammymoore07@gmail.com>
2016-11-01 13:43:33 -1000

    academic year version

--- app/imports/ui/components/planner/academic-semester.js
+++ app/imports/ui/components/planner/academic-semester.js
@@ -1,8 +1,70 @@
 import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
+import { Tracker } from 'meteor/tracker';
 import { ReactiveDict } from 'meteor/reactive-dict';
+import { lodash } from 'meteor/erasaur:meteor-lodash';
+import { $ } from 'meteor/jquery';
 import { Semesters } from '../../../api/semester/SemesterCollection.js';
 import { CourseInstances } from '../../../api/course/CourseInstanceCollection.js';
+import { Courses } from '../../../api/course/CourseCollection.js';
+
+const availableCourses = () => {
+  const courses = Courses.find({}).fetch();
+  if (courses.length > 0 && Template.instance().state.get('semester')) {
+    const filtered = lodash.filter(courses, function (course) {
+      if (course.number === 'ICS 499') {
+        return true;
+      }
+      const ci = CourseInstances.find({
+        studentID: Meteor.userId(),
+        courseID: course._id,
+      }).fetch();
+      return ci.length === 0;
+    });
+    return filtered;
+  }
+  return [];
+};
+
+const available1xxCourses = () => {
+  const courses = availableCourses();
+  const filtered = lodash.filter(courses, function (course) {
+    return course.number.substring(0, 5) === 'ICS 1';
+  });
+  return filtered;
+};
+
+const available2xxCourses = () => {
+  const courses = availableCourses();
+  const filtered = lodash.filter(courses, function (course) {
+    return course.number.substring(0, 5) === 'ICS 2';
+  });
+  return filtered;
+};
+
+const available3xxCourses = () => {
+  const courses = availableCourses();
+  const filtered = lodash.filter(courses, function (course) {
+    return course.number.substring(0, 5) === 'ICS 3';
+  });
+  return filtered;
+};
+
+const available4xxCourses = () => {
+  const courses = availableCourses();
+  const filtered = lodash.filter(courses, function (course) {
+    return course.number.substring(0, 5) === 'ICS 4';
+  });
+  return filtered;
+};
+
+const resizePopup = () => {
+  $('.ui.popup').css('max-height', '350px');
+};
+
+$(window).resize(function (e) {
+  resizePopup();
+});
 
 Template.Academic_Semester.helpers({
   semesterName() {
@@ -42,6 +104,58 @@ Template.Academic_Semester.helpers({
     }
     return ret;
   },
+  hasCourses(level) {
+    let ret = false;
+    switch (level) {
+      case 100:
+        ret = available1xxCourses().length !== 0;
+        break;
+      case 200:
+        ret = available2xxCourses().length !== 0;
+        break;
+      case 300:
+        ret = available3xxCourses().length !== 0;
+        break;
+      case 400:
+        ret = available4xxCourses().length !== 0;
+        break;
+      default:
+        ret = false;
+    }
+    return ret;
+  },
+  courses(level) {
+    const ret = [];
+    const courses = availableCourses();
+    courses.forEach((course) => {
+      const cNumber = course.number;
+      switch (level) {
+        case 100:
+          if (cNumber.substring(0, 5) === 'ICS 1') {
+            ret.push(cNumber);
+          }
+          break;
+        case 200:
+          if (cNumber.substring(0, 5) === 'ICS 2') {
+            ret.push(cNumber);
+          }
+          break;
+        case 300:
+          if (cNumber.substring(0, 5) === 'ICS 3') {
+            ret.push(cNumber);
+          }
+          break;
+        case 400:
+          if (cNumber.substring(0, 5) === 'ICS 4') {
+            ret.push(cNumber);
+          }
+          break;
+        default:
+          break;
+      }
+    });
+    return ret;
+  },
   nonIcsCourses() {
     const ret = [];
     if (Template.instance().state.get('semester')) {
@@ -57,8 +171,7 @@ Template.Academic_Semester.helpers({
     }
     return ret;
   },
-})
-;
+});
 
 Template.Academic_Semester.events({
   'drop .bodyDrop'(event) {
@@ -75,6 +188,11 @@ Template.Academic_Semester.events({
       }
     }
   },
+  'click .item.addClass'(event) {
+    event.preventDefault();
+    console.log(event.target.text);
+    Template.instance().$('a.item.100').popup('hide all');
+  },
 });
 
 Template.Academic_Semester.onCreated(function academicSemesterOnCreate() {
@@ -87,6 +205,45 @@ Template.Academic_Semester.onRendered(function academicSemesterOnRendered() {
     this.state.set('semester', this.data.semester);
     this.state.set('currentSemester', this.data.currentSemester);
   }
+  const template = this;
+  Tracker.afterFlush(() => {
+    template.$('.ui.icon.mini.button')
+        .popup({
+          on: 'click',
+        });
+    template.$('.item.course')
+        .popup({
+          inline: true,
+          hoverable: true,
+        });
+    template.$('.courseInstance').popup({
+      inline: true, hoverable: true, position: 'right center',
+    });
+    template.$('a.100.item')
+        .popup({
+          inline: true,
+          hoverable: true,
+        });
+    template.$('a.200.item')
+        .popup({
+          inline: true,
+          hoverable: true,
+        });
+    template.$('a.300.item')
+        .popup({
+          inline: true,
+          hoverable: true,
+        });
+    template.$('a.400.item')
+        .popup({
+          inline: true,
+          hoverable: true,
+          lastResort: 'right center',
+          onShow: function () {
+            resizePopup();
+          },
+        });
+  })
 });
 
 Template.Academic_Semester.onDestroyed(function acdemicSemesterOnDestroyed() {
