933a441528e8e80feaa84e92aaabc4f26e99955f
Cam Moore <cammymoore07@gmail.com>
2016-12-02 11:09:55 -1000

    Moved the example students and admin accounts.
    Moved from settings.development.json to /imports/startup/server/icsdata.

---
+++ app/imports/startup/server/icsdata/AdminUser.js
@@ -0,0 +1,14 @@
+/**
+ * The default admin account.
+ * @type {{firstName: string, lastName: string, email: string, slug: string,
+ * role: string, password: string, uhID: string}}
+ */
+export const defaultAdminAccount = {
+  firstName: 'Admin',
+  lastName: 'RadGrad',
+  email: 'radgrad@hawaii.edu',
+  slug: 'radgrad',
+  role: 'ADMIN',
+  password: 'foo',
+  uhID: '1111-1111',
+};
