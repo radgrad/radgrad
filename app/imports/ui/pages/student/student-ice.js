7abd043984b6944b19159de17bec93ce83f45c33
Amy Takayesu <amytakayesu@gmail.com>
2016-12-03 14:40:27 -1000

    Added accordians to ICE page

--- app/imports/ui/pages/student/student-ice.js
+++ app/imports/ui/pages/student/student-ice.js
@@ -1,3 +1,12 @@
 /**
  * Created by ataka on 11/7/16.
  */
+
+Template.Student_Ice.onRendered(function enableAccordian() {
+  this.$('.accordion').accordion({
+    selector: {
+      trigger: '.title .icon'
+    }
+  })
+
+});
