a2448db044d8c201347b195a96b0328f63d13b0e
Philip Johnson <johnson@hawaii.edu>
2016-12-04 13:36:43 -1000

    Finished initial mockup of help pane

--- app/imports/ui/components/student/index.js
+++ app/imports/ui/components/student/index.js
@@ -1,6 +1,12 @@
 import './career-goals.html';
 import './career-goals.js';
 
+import './explorer-dropdown.html';
+import './explorer-dropdown.js';
+
+import './home-menu.html';
+import './home-menu.js';
+
 import './ice.css';
 import './ice.html';
 import './ice.js';
@@ -8,12 +14,18 @@ import './ice.js';
 import './interests.html';
 import './interests.js';
 
+import './student-home-help.html';
+import './student-home-help.js';
+
+import './student-ice-help.html';
+import './student-ice-help.js';
+
 import './warn-recommend.html';
 import './warn-recommend.js';
 
-import './explorer-dropdown.html';
-import './explorer-dropdown.js';
 
-import './home-menu.html';
-import './home-menu.js';
+
+
+
+
 
