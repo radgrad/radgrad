4fb294f0ea7df18a5e2cf9baa8ca8fcfdde40440
Philip Johnson <johnson@hawaii.edu>
2016-12-06 13:50:22 -1000

    checkpoint

--- app/imports/ui/components/admin/admin-datamodel-menu.js
+++ app/imports/ui/components/admin/admin-datamodel-menu.js
@@ -1,3 +1,8 @@
-/**
- * Created by ataka on 11/7/16.
- */
+import { Template } from 'meteor/templating';
+import { adminDataModelCareerGoalsPageRouteName } from '/imports/startup/client/router.js';
+
+Template.Admin_DataModel_Menu.helpers({
+  careerGoalsRouteName() {
+    return adminDataModelCareerGoalsPageRouteName;
+  },
+});
