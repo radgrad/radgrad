97256c603055a21187d8412805070e08df8d1278
Amy Takayesu <amytakayesu@gmail.com>
2016-12-11 20:17:51 -1000

    Started backend for Student Profile page
    I should have done this in branch 47

--- app/imports/ui/pages/student/student-aboutme.js
+++ app/imports/ui/pages/student/student-aboutme.js
@@ -1,17 +1,132 @@
+import { FlowRouter } from 'meteor/kadira:flow-router';
+import { ReactiveDict } from 'meteor/reactive-dict';
 import { Template } from 'meteor/templating';
+import { _ } from 'meteor/erasaur:meteor-lodash';
 
-Template.Student_AboutMe.onCreated(function appBodyOnCreated() {
-  // placeholder: typically you will put global subscriptions here if you remove the autopublish package.
-});
+import { SessionState, sessionKeys, updateSessionState } from '../../../startup/client/session-state';
+import { AcademicYearInstances } from '../../../api/year/AcademicYearInstanceCollection';
+import { CareerGoals } from '../../../api/career/CareerGoalCollection';
+import { CourseInstances } from '../../../api/course/CourseInstanceCollection';
+import { BS_CS_TEMPLATE, BA_ICS_TEMPLATE } from '../../../api/degree-program/degree-program';
+import { generateCoursePlan } from '../../../api/degree-program/plan-generator';
+import { Interests } from '../../../api/interest/InterestCollection';
+import { Semesters } from '../../../api/semester/SemesterCollection';
+import * as semUtils from '../../../api/semester/SemesterUtilities';
+import * as courseUtils from '../../../api/course/CourseFunctions';
+import { Users } from '../../../api/user/UserCollection.js';
+import { studentDegreePlannerPageRouteName } from '../../../startup/client/router';
 
 Template.Student_AboutMe.helpers({
-  // placeholder: if you display dynamic data in your layout, you will put your template helpers here.
+  careerGoals() {
+    const ret = [];
+    if (SessionState.get(sessionKeys.CURRENT_STUDENT_ID)) {
+      const user = Users.findDoc(SessionState.get(sessionKeys.CURRENT_STUDENT_ID));
+      _.map(user.careerGoalIDs, (id) => {
+        ret.push(CareerGoals.findDoc(id));
+    });
+    }
+    return ret;
+  },
+  currentSemesterName() {
+    const currentSemesterID = Semesters.getCurrentSemester();
+    return Semesters.toString(currentSemesterID, false);
+  },
+  desiredDegree() {
+    if (SessionState.get(sessionKeys.CURRENT_STUDENT_ID)) {
+      const user = Users.findDoc(SessionState.get(sessionKeys.CURRENT_STUDENT_ID));
+      if (user.desiredDegree === 'BS_CS') {
+        return 'B.S. CS';
+      } else if (user.desiredDegree === 'BA_ICS') {
+        return 'B.A. ICS';
+      }
+    }
+    return '';
+  },
+  futureSemesters() {
+    const currentSemester = Semesters.getCurrentSemesterDoc();
+    return _.filter(Semesters.find({ sortBy: { $gte: currentSemester.sortBy } }, { sort: { sortBy: 1 } }).fetch(),
+        function notSummer(s) {
+          return s.term !== Semesters.SUMMER;
+        });
+  },
+  interests() {
+    const ret = [];
+    if (SessionState.get(sessionKeys.CURRENT_STUDENT_ID)) {
+      const user = Users.findDoc(SessionState.get(sessionKeys.CURRENT_STUDENT_ID));
+      _.map(user.interestIDs, (id) => {
+        ret.push(Interests.findDoc(id));
+    });
+    }
+    return ret;
+  },
+  selectedSemester(semester) {
+    return Template.instance().state.get('selectedSemester') === semester;
+  },
+  semesterName(semester) {
+    return Semesters.toString(semester._id, false);
+  },
+  semesterSlug(semester) {
+    return Semesters.getSlug(semester._id);
+  },
+  userFullName() {
+    if (SessionState.get(sessionKeys.CURRENT_STUDENT_ID)) {
+      const user = Users.findDoc(SessionState.get(sessionKeys.CURRENT_STUDENT_ID));
+      return Users.getFullName(user._id);
+    }
+    return 'Invalid Student';
+  },
 });
 
 Template.Student_AboutMe.events({
-  // placeholder: if you add a form to this top-level layout, handle the associated events here.
+  'click .jsSemester': function clickJsInterests(event, instance) {
+    event.preventDefault();
+    const choice = event.target.parentElement.getElementsByTagName('input')[0].value;
+    const id = Semesters.getID(choice);
+    instance.state.set('selectedSemester', Semesters.findDoc(id));
+  },
+  'click .jsGeneratePlan': function clickGeneratePlan(event, instance) {
+    event.preventDefault();
+    const studentID = SessionState.get(sessionKeys.CURRENT_STUDENT_ID);
+    const student = Users.findDoc(studentID);
+    let template;
+    if (student.desiredDegree === 'BS_CS') {
+      template = BS_CS_TEMPLATE;
+    }
+    if (student.desiredDegree === 'BA_ICS') {
+      template = BA_ICS_TEMPLATE;
+    }
+    const currentSemester = Semesters.getCurrentSemesterDoc();
+    let startSemester = instance.state.get('selectedSemester');
+    if (currentSemester.sortBy === startSemester.sortBy) {
+      startSemester = semUtils.nextFallSpringSemester(startSemester);
+    }
+    // TODO: CAM do we really want to blow away the student's plan. What if they've made changes?
+    courseUtils.clearPlannedCourseInstances(studentID);
+    const cis = CourseInstances.find({ studentID }).fetch();
+    const ays = AcademicYearInstances.find({ studentID }).fetch();
+    if (cis.length === 0) {
+      _.map(ays, (year) => {
+        AcademicYearInstances.removeIt(year._id);
+    });
+    } else {
+      // TODO: CAM figure out which AYs to remove.
+    }
+
+    generateCoursePlan(template, startSemester, student);
+    FlowRouter.go(studentDegreePlannerPageRouteName);
+  },
 });
 
-Template.Student_AboutMe.onRendered( {
-
+Template.Student_AboutMe.onCreated(function studentAboutMeOnCreated() {
+  this.state = new ReactiveDict();
+  updateSessionState(SessionState);
 });
+
+Template.Student_AboutMe.onRendered(function studentAboutMeOnRendered() {
+  //add your statement here
+});
+
+Template.Student_AboutMe.onDestroyed(function studentAboutMeOnDestroyed() {
+  // add your statement here
+});
+
