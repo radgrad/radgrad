07e77a942d763eab902026b93638e5cabbc6ae36
Philip Johnson <johnson@hawaii.edu>
2016-12-13 16:41:13 -1000

    checkpoint.

--- app/imports/ui/components/admin/list-career-goals-widget.js
+++ app/imports/ui/components/admin/list-career-goals-widget.js
@@ -1,4 +1,12 @@
+import { Template } from 'meteor/templating';
+import { CareerGoals } from '../../../api/career/CareerGoalCollection';
 
+Template.List_Career_Goals_Widget.onCreated(function listCareerGoalsWidgetOnCreated() {
+  this.subscribe(CareerGoals.getPublicationName());
+});
 
-
-
+Template.List_Career_Goals_Widget.helpers({
+  careerGoals() {
+    return CareerGoals.find();
+  },
+})
