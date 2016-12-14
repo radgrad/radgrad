f2e84c0aaa7337f4560da03cabd4aee5d61dff4d
Philip Johnson <johnson@hawaii.edu>
2016-12-14 07:00:56 -1000

    checkpoint.

--- app/imports/ui/components/admin/list-career-goals-widget.js
+++ app/imports/ui/components/admin/list-career-goals-widget.js
@@ -1,12 +1,28 @@
 import { Template } from 'meteor/templating';
 import { CareerGoals } from '../../../api/career/CareerGoalCollection';
+import { Interests } from '../../../api/interest/InterestCollection';
+import { Slugs } from '../../../api/slug/SlugCollection';
+import { Users } from '../../../api/user/UserCollection';
+import { _ } from 'meteor/erasaur:meteor-lodash';
 
 Template.List_Career_Goals_Widget.onCreated(function listCareerGoalsWidgetOnCreated() {
   this.subscribe(CareerGoals.getPublicationName());
+  this.subscribe(Interests.getPublicationName());
+  this.subscribe(Users.getPublicationName());
+  this.subscribe(Slugs.getPublicationName());
 });
 
 Template.List_Career_Goals_Widget.helpers({
   careerGoals() {
     return CareerGoals.find();
   },
-})
+  careerGoalsCount() {
+    return CareerGoals.count();
+  },
+  getInterestNames(interestIDs) {
+    return Interests.findNames(interestIDs);
+  },
+  getSlugName(slugID) {
+    return Slugs.findDoc(slugID).name;
+  }
+});
