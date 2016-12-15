9db073d3e4f3569e1005e92d6eda4bec24126334
Amy Takayesu <amytakayesu@gmail.com>
2016-12-14 20:40:12 -1000

    Cleaned up student profile .js file code
    Still some problems. Will return later

--- app/imports/ui/pages/student/student-aboutme.js
+++ app/imports/ui/pages/student/student-aboutme.js
@@ -8,22 +8,14 @@ import { moment } from 'meteor/momentjs:moment';
 
 
 import { SessionState, sessionKeys, updateSessionState } from '../../../startup/client/session-state';
-import { AcademicYearInstances } from '../../../api/year/AcademicYearInstanceCollection';
 import { CareerGoals } from '../../../api/career/CareerGoalCollection';
 import { Courses } from '../../../api/course/CourseCollection';
 import { CourseInstances } from '../../../api/course/CourseInstanceCollection';
-import { Feedbacks } from '../../../api/feedback/FeedbackCollection';
-import { FeedbackInstances } from '../../../api/feedback/FeedbackInstanceCollection';
-import { BS_CS_TEMPLATE, BA_ICS_TEMPLATE } from '../../../api/degree-program/degree-program';
 import { Interests } from '../../../api/interest/InterestCollection';
 import { Opportunities } from '../../../api/opportunity/OpportunityCollection';
 import { OpportunityInstances } from '../../../api/opportunity/OpportunityInstanceCollection.js';
 import { Semesters } from '../../../api/semester/SemesterCollection';
-import * as semUtils from '../../../api/semester/SemesterUtilities';
-import * as courseUtils from '../../../api/course/CourseFunctions';
 import { Users } from '../../../api/user/UserCollection.js';
-import { VerificationRequests } from '../../../api/verification/VerificationRequestCollection.js';
-import { Slugs } from '../../../api/slug/SlugCollection.js';
 
 
 Template.Student_AboutMe.helpers({
@@ -55,6 +47,13 @@ Template.Student_AboutMe.helpers({
     }
     return '';
   },
+  getName() {
+    if (SessionState.get(sessionKeys.CURRENT_STUDENT_ID)) {
+      const user = Users.findDoc(SessionState.get(sessionKeys.CURRENT_STUDENT_ID));
+      return user.firstName + ' ' + user.lastName;
+    }
+    return '';
+  },
   getEmail() {
     if (SessionState.get(sessionKeys.CURRENT_STUDENT_ID)) {
       const user = Users.findDoc(SessionState.get(sessionKeys.CURRENT_STUDENT_ID));
@@ -69,13 +68,6 @@ Template.Student_AboutMe.helpers({
     }
     return '';
   },
-  futureSemesters() {
-    const currentSemester = Semesters.getCurrentSemesterDoc();
-    return _.filter(Semesters.find({ sortBy: { $gte: currentSemester.sortBy } }, { sort: { sortBy: 1 } }).fetch(),
-        function notSummer(s) {
-          return s.term !== Semesters.SUMMER;
-        });
-  },
   interests() {
     const ret = [];
     if (SessionState.get(sessionKeys.CURRENT_STUDENT_ID)) {
@@ -87,24 +79,7 @@ Template.Student_AboutMe.helpers({
     }
     return ret;
   },
-  selectedSemester(semester) {
-    return Template.instance().state.get('selectedSemester') === semester;
-  },
-  semesterName(semester) {
-    return Semesters.toString(semester._id, false);
-  },
-  semesterSlug(semester) {
-    return Semesters.getSlug(semester._id);
-  },
-  userFullName() {
-    if (SessionState.get(sessionKeys.CURRENT_STUDENT_ID)) {
-      const user = Users.findDoc(SessionState.get(sessionKeys.CURRENT_STUDENT_ID));
-      return Users.getFullName(user._id);
-    }
-    return 'Invalid Student';
-  },
 
-  /*Academic plan helpers*/
   /*Returns all course instances in student's plan*/
   courses() {
     let ret = [];
@@ -154,76 +129,6 @@ Template.Student_AboutMe.helpers({
     const oppYear = sem.year;
     return oppTerm + ' ' + oppYear;
   },
-  requestStatus() {
-    if (Template.instance().state.get('detailOpportunityID')) {
-      const id = Template.instance().state.get('detailOpportunityID');
-      if (OpportunityInstances.isDefined(id)) {
-        const oppInstance = OpportunityInstances.findDoc(id);
-        const request = VerificationRequests.find({ opportunityInstanceID: oppInstance._id }).fetch();
-        if (request.length > 0) {
-          return request[0].status;
-        }
-      }
-    }
-    return '';
-  },
-  requestWhenSubmitted() {
-    if (Template.instance().state.get('detailOpportunityID')) {
-      const id = Template.instance().state.get('detailOpportunityID');
-      if (OpportunityInstances.isDefined(id)) {
-        const oppInstance = OpportunityInstances.findDoc(id);
-        const request = VerificationRequests.find({ opportunityInstanceID: oppInstance._id }).fetch();
-        if (request.length > 0) {
-          const submitted = moment(request[0].submittedOn);
-          return submitted.calendar();
-        }
-      }
-    }
-    return '';
-  },
-  springArgs(year) {
-    if (Template.instance().state.get('currentSemesterID')) {
-      const currentSemesterID = Template.instance().state.get('currentSemesterID');
-      const currentSemester = Semesters.findDoc(currentSemesterID);
-      const semesterID = Semesters.define({
-        year: year.year + 1,
-        term: Semesters.SPRING,
-      });
-      const semester = Semesters.findDoc(semesterID);
-      return { currentSemester, semester };
-    }
-    return null;
-  },
-  summerArgs(year) {
-    if (Template.instance().state.get('currentSemesterID')) {
-      const currentSemesterID = Template.instance().state.get('currentSemesterID');
-      const currentSemester = Semesters.findDoc(currentSemesterID);
-      const semesterID = Semesters.define({
-        year: year.year + 1,
-        term: Semesters.SUMMER,
-      });
-      const semester = Semesters.findDoc(semesterID);
-      return { currentSemester, semester };
-    }
-    return null;
-  },
-
-  years() {
-    const studentID = SessionState.get(sessionKeys.CURRENT_STUDENT_ID);
-    const ay = AcademicYearInstances.find({ studentID }, { sort: { year: 1 } }).fetch();
-    const instance = Template.instance();
-    if (ay.length > 0 && !instance.state.get('startYear')) {
-      instance.state.set('startYear', ay[ay.length - 1].year);
-    }
-    const ret = lodash.filter(ay, function filter(academicYear) {
-      const year = academicYear.year;
-      if (year >= instance.state.get('startYear') - 3 && year <= instance.state.get('startYear')) {
-        return true;
-      }
-      return false;
-    });
-    return ret;
-  },
 });
 
 Template.Student_AboutMe.events({
@@ -256,18 +161,14 @@ Template.Student_AboutMe.onCreated(function studentAboutMeOnCreated() {
     console.log('there is a problem no data.'); // eslint-disable-line no-console
   }
   this.autorun(() => {
-    this.subscribe(AcademicYearInstances.getPublicationName());
   this.subscribe(CareerGoals.getPublicationName());
   this.subscribe(Courses.getPublicationName());
   this.subscribe(CourseInstances.getPublicationName());
-  this.subscribe(FeedbackInstances.getPublicationName());
-  this.subscribe(Feedbacks.getPublicationName());
   this.subscribe(Interests.getPublicationName());
   this.subscribe(Opportunities.getPublicationName());
   this.subscribe(OpportunityInstances.getPublicationName());
   this.subscribe(Semesters.getPublicationName());
   this.subscribe(Users.getPublicationName());
-  this.subscribe(VerificationRequests.getPublicationName());
 });
 });
 
