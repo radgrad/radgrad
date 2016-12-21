027911b1c3b845b9ac8aa8b08dbdd37068947670
Amy Takayesu <amytakayesu@gmail.com>
2016-12-20 22:48:59 -1000

    Radgrad community activity and teasers are scollable

--- app/imports/ui/components/student/index.js
+++ app/imports/ui/components/student/index.js
@@ -14,23 +14,26 @@ import './ice.js';
 import './interests.html';
 import './interests.js';
 
-import './student-courses-of-interest-widget.html';
-import './student-courses-of-interest-widget.js';
-
-import './student-opportunities-of-interest-widget.html';
-import './student-opportunities-of-interest-widget.js';
-
 import './student-content-of-interest-card.html';
 import './student-content-of-interest-card.js';
 
 import './student-courses-of-interest-card.html';
 import './student-courses-of-interest-card.js';
 
+import './student-courses-of-interest-widget.html';
+import './student-courses-of-interest-widget.js';
+
+import './student-feed-widget.html';
+import './student-feed-widget.js';
+
 import './student-home-help.html';
 import './student-home-help.js';
 
 import './student-ice-help.html';
 import './student-ice-help.js';
+
+import './student-opportunities-of-interest-widget.html';
+import './student-opportunities-of-interest-widget.js';
 
 import './student-teaser-widget.html';
 import './student-teaser-widget.js';
