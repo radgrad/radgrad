2edc56d270a3755e8b8e09e30a47fe5092cfaf88
Cam Moore <cammymoore07@gmail.com>
2016-12-21 14:40:14 -1000

    Starting on Advisor Page redesign

---
+++ app/imports/ui/components/advisor/add-student-widget.js
@@ -0,0 +1,25 @@
+/**
+ * Created by Cam Moore on 12/21/16.
+ */
+import { Template } from 'meteor/templating';
+
+Template.Add_Student_Widget.helpers({
+  // add your helpers here
+});
+
+Template.Add_Student_Widget.events({
+  // add your events here
+});
+
+Template.Add_Student_Widget.onCreated(function addStudentWidgetOnCreated() {
+  // add your statement here
+});
+
+Template.Add_Student_Widget.onRendered(function addStudentWidgetOnRendered() {
+  // add your statement here
+});
+
+Template.Add_Student_Widget.onDestroyed(function addStudentWidgetOnDestroyed() {
+  // add your statement here
+});
+
