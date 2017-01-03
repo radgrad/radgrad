13383d557be31b2941acddffb18309d803a262d3
Aljon Preza <aljonpreza@yahoo.com>
2017-01-02 17:05:33 -1000

    Added ICE format to degree plan
    Also made small fixes to menu

--- app/imports/ui/components/landing/index.js
+++ app/imports/ui/components/landing/index.js
@@ -2,6 +2,9 @@ import './ice-badge.css';
 import './ice-badge.html';
 import './ice-badge.js';
 
+import './ice-badge-proj.html';
+import './ice-badge-proj.js';
+
 import './landing-section-1.css';
 import './landing-section-1.html';
 import './landing-section-1.js';
