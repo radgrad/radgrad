0249f3523861318c29534b9b258ce802c4399ce2
Amy Takayesu <amytakayesu@gmail.com>
2017-01-05 11:36:51 -1000

    Fixed feed test errors
    Feeds now have slugs.

--- app/imports/ui/components/student/student-explorer-list-career-goals-widget.js
+++ app/imports/ui/components/student/student-explorer-list-career-goals-widget.js
@@ -17,7 +17,7 @@ Template.Student_Explorer_List_Career_Goals_Widget.onCreated(function onCreated(
 });
 
 
-function interestedUsers (careerGoal, role) {
+function interestedUsers(careerGoal, role) {
   const interested = [];
   const users = Users.find({ roles: [role] }).fetch();
   _.map(users, (user) => {
@@ -47,12 +47,12 @@ Template.Student_Explorer_List_Career_Goals_Widget.helpers({
       { label: 'Description', value: careerGoal.description },
       { label: 'More Information', value: makeLink(careerGoal.moreInformation) },
       { label: 'Interests', value: _.sortBy(Interests.findNames(careerGoal.interestIDs)) },
-      { label: 'student(s)', value: numUsers(careerGoal, ROLE.STUDENT), type: 'amount'},
-      { label: 'Students', value: interestedUsers(careerGoal, ROLE.STUDENT), type: 'list'},
-      { label: 'faculty member(s)', value: numUsers(careerGoal, ROLE.FACULTY), type: 'amount'},
-      { label: 'Faculty Members', value: interestedUsers(careerGoal, ROLE.FACULTY), type: 'list'},
-      { label: 'alumni', value: numUsers(careerGoal, ROLE.ALUMNI), type: 'amount'},
-      { label: 'Alumni', value: interestedUsers(careerGoal, ROLE.ALUMNI), type: 'list'},
+      { label: 'student(s)', value: numUsers(careerGoal, ROLE.STUDENT), type: 'amount' },
+      { label: 'Students', value: interestedUsers(careerGoal, ROLE.STUDENT), type: 'list' },
+      { label: 'faculty member(s)', value: numUsers(careerGoal, ROLE.FACULTY), type: 'amount' },
+      { label: 'Faculty Members', value: interestedUsers(careerGoal, ROLE.FACULTY), type: 'list' },
+      { label: 'alumni', value: numUsers(careerGoal, ROLE.ALUMNI), type: 'amount' },
+      { label: 'Alumni', value: interestedUsers(careerGoal, ROLE.ALUMNI), type: 'list' },
     ];
   },
 });
