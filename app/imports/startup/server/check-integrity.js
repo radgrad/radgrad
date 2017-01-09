674766f8021d523c6484a96d1cbf938991ae3eef
Philip Johnson <johnson@hawaii.edu>
2017-01-09 11:26:10 -1000

    Only print integrity messages to console on startup if a problem was found

--- app/imports/startup/server/check-integrity.js
+++ app/imports/startup/server/check-integrity.js
@@ -1,4 +1,7 @@
 import { checkIntegrity } from '/imports/api/integritychecker/IntegrityChecker';
 
-// Invoke checkIntegrity on startup.
-console.log(checkIntegrity().message);
+// Invoke checkIntegrity on startup, print out message if integrity issues were found.
+const integrity = checkIntegrity();
+if (integrity.count > 0) {
+  console.log(checkIntegrity().message);
+}
