fdd9144c1d6d3f22462f18cdc02557dbfbc87cf2
jogarces <josebeann@gmail.com>
2017-01-14 16:57:18 -1000

    Submitting new questions is still a wip!

--- app/imports/startup/server/icsdata/MentorSpaceQuestionsDefinitions.js
+++ app/imports/startup/server/icsdata/MentorSpaceQuestionsDefinitions.js
@@ -10,21 +10,26 @@ export const mentorspaceQuestionsDefinitions = [
   {
     title: 'What do you look for when hiring a new graduate?',
     slug: 'hiring-expectations',
+    approved: true,
   },
   {
     title: 'What mistakes do CS students make during interviews?',
     slug: 'interview-mistakes',
+    approved: true,
   },
   {
     title: 'What aspects of your undergraduate degree experience has proven most useful to you?',
     slug: 'undergrad-experience',
+    approved: true,
   },
   {
     title: 'What do CS students need to know about tech careers in your region?',
     slug: 'tech-career',
+    approved: true,
   },
   {
     title: 'I\'m interested in a career in data science. What should I be doing as an undergrad to prepare?',
     slug: 'data-science-prep',
+    approved: true,
   },
 ];
