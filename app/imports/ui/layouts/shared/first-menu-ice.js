bcf3d25b56d4ca463121667a466b0934c556a545
Amy Takayesu <amytakayesu@gmail.com>
2017-01-26 22:09:44 -1000

    ICE circles don't display over 100

--- app/imports/ui/layouts/shared/first-menu-ice.js
+++ app/imports/ui/layouts/shared/first-menu-ice.js
@@ -4,4 +4,7 @@ Template.First_Menu_Ice.helpers({
   pClass(value) {
     return (value <= 100) ? `p${value}` : 'p100';
   },
+  finalEarned(value) {
+    return (value <= 100) ? value : 100;
+  },
 });
