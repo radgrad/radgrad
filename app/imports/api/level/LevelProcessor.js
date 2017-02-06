2d5e8d712f83f1f79e61a476b81701b33ac066a2
Cam Moore <cammymoore07@gmail.com>
2017-02-06 10:08:11 -1000

    Removed TODO

--- app/imports/api/level/LevelProcessor.js
+++ app/imports/api/level/LevelProcessor.js
@@ -11,7 +11,7 @@ export function calcLevel(studentID) {
       OpportunityInstances.find({ studentID }).fetch());
   const verified = [];
   _.map(instances, (i) => {
-    if (i.verified) { // TODO need to not add taking the class again.
+    if (i.verified) { // May need to account for taking the class again.
       verified.push(i);
     }
   });
