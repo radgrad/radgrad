1fc9807e9276e0132bb50a8e1e1c3831b2baf96c
Cam Moore <cammymoore07@gmail.com>
2017-02-11 09:16:03 -1000

    Checks the degree plan.

--- app/imports/api/feedback/FeedbackFunctions.js
+++ app/imports/api/feedback/FeedbackFunctions.js
@@ -1,4 +1,5 @@
 // import { check } from 'meteor/check';
+import { FlowRouter } from 'meteor/kadira:flow-router';
 import { _ } from 'meteor/erasaur:meteor-lodash';
 import { CourseInstances } from '../course/CourseInstanceCollection';
 import { Courses } from '../course/CourseCollection';
@@ -9,6 +10,7 @@ import { Semesters } from '../semester/SemesterCollection';
 import * as oppUtils from '../opportunity/OpportunityUtilities';
 import { Slugs } from '../slug/SlugCollection';
 import { Users } from '../user/UserCollection';
+import { BS_CS_LIST, BA_ICS_LIST } from '../degree-program/degree-program';
 
 /** @module FeedbackFunctions */
 
@@ -58,7 +60,7 @@ export class FeedbackFunctionClass {
   checkPrerequisites(studentID) {
     // console.log('checkPrerequisites');
     const f = Feedbacks.find({ name: 'Prerequisite missing' }).fetch()[0];
-    const feedback = Slugs.getEntityID(f.slugID, 'Feedback');
+    const feedback = Slugs.getNameFromID(f.slugID, 'Feedback');
     const area = `ffn-${feedback}`;
     this.clearFeedbackInstances(studentID, area);
     const cis = CourseInstances.find({ studentID }).fetch();
@@ -112,26 +114,57 @@ export class FeedbackFunctionClass {
    */
   checkCompletePlan(studentID) {
     // console.log('checkCompletePlan');
-    const f = Feedbacks.find({ name: 'Required course missing' }).fetch()[0];
-    const feedback = Slugs.getEntityID(f.slugID, 'Feedback');
+    const f = Feedbacks.findDoc({ name: 'Required course missing' });
+    const feedback = Slugs.getNameFromID(f.slugID, 'Feedback');
     const area = `ffn-${feedback}`;
     this.clearFeedbackInstances(studentID, area);
     const student = Users.findDoc(studentID);
     const courseIDs = Users.getCourseIDs(studentID);
     const degree = DesiredDegrees.findDoc({ _id: student.desiredDegreeID });
+    let courses;
     if (degree.shortName.startsWith('B.S.')) {
-      _.map(courseIDs, (id) => {
-        const course = Courses.findDoc(id);
-        const slug = Slugs.findDoc(course.slugID);
-        console.log(slug.name);
-      });
+      courses = BS_CS_LIST.slice(0);
     }
     if (degree.shortName.startsWith('B.A.')) {
-      _.map(courseIDs, (id) => {
-        console.log(Courses.findDoc(id).shortName);
+      courses = BA_ICS_LIST.slice(0);
+    }
+    _.map(courseIDs, (id) => {
+      const course = Courses.findDoc(id);
+      const slug = Slugs.getNameFromID(course.slugID);
+      const index = _.indexOf(courses, slug);
+      // console.log(slug, index);
+      if (index !== -1) {
+        courses.splice(index, 1);
+      } else if (slug.startsWith('ics4')) {
+        courses.splice(_.indexOf(courses, 'ics4xx'), 1);
+      } else if (_.indexOf(courses[0], slug)) {
+        courses.splice(0, 1);
+      } else if (_.indexOf(courses[1], slug)) {
+        courses.splice(1, 1);
+      }
+    });
+    if (courses.length > 0) {
+      let description = 'Your degree plan is missing: ';
+      const currentRoute = FlowRouter.current().path;
+      const index = currentRoute.lastIndexOf('/');
+      const basePath = currentRoute.substring(0, index + 1);
+      _.map(courses, (slug) => {
+        if (slug.startsWith('ics4')) {
+          description = `${description} \n- a 400 level elective, `;
+        } else {
+          const id = Slugs.getEntityID(slug, 'Course');
+          const course = Courses.findDoc(id);
+          description = `${description} \n- [${course.shortName}](${basePath}explorer/courses/${slug}), `;
+        }
+      });
+      description = description.substring(0, description.length - 2);
+      FeedbackInstances.define({
+        feedback,
+        user: studentID,
+        description,
+        area,
       });
     }
-
   }
 
   /**
@@ -140,9 +173,12 @@ export class FeedbackFunctionClass {
    */
   generateRecommendedCurrentSemesterOpportunities(studentID) {
     const feedbackDoc = Feedbacks.findDoc({ name: 'Opportunity recommendations based on interests' });
-    const feedback = Slugs.getEntityID(feedbackDoc.slugID, 'Feedback');
+    const feedback = Slugs.getNameFromID(feedbackDoc.slugID, 'Feedback');
     const area = `ffn-${feedback}`;
     let bestChoices = oppUtils.getStudentCurrentSemesterOpportunityChoices(studentID);
+    const currentRoute = FlowRouter.current().path;
+    const index = currentRoute.lastIndexOf('/');
+    const basePath = currentRoute.substring(0, index + 1);
     // console.log(bestChoices);
     if (bestChoices) {
       const len = bestChoices.length;
@@ -152,7 +188,8 @@ export class FeedbackFunctionClass {
       this.clearFeedbackInstances(studentID, area);
       let description = 'Consider the following opportunities for this semester: ';
       _.map(bestChoices, (opp) => {
-        description = `${description} ${opp.name}, `;
+        const slug = Slugs.findDoc(opp.slugID);
+        description = `${description} \n- [${opp.name}](${basePath}explorer/opportunities/${slug.name}), `;
       });
       description = description.substring(0, description.length - 2);
       FeedbackInstances.define({
