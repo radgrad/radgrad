9506a1855db2b887d6b67b9e59c072e7d75704ec
Philip Johnson <johnson@hawaii.edu>
2017-02-18 16:15:03 -1000

    Fiddle with landing page.

--- app/imports/ui/components/landing/landing-section-2.js
+++ app/imports/ui/components/landing/landing-section-2.js
@@ -3,28 +3,37 @@ import { Template } from 'meteor/templating';
 import { Courses } from '../../../api/course/CourseCollection.js';
 import { Interests } from '../../../api/interest/InterestCollection.js';
 import { Opportunities } from '../../../api/opportunity/OpportunityCollection.js';
+import { CareerGoals } from '../../../api/career/CareerGoalCollection.js';
 
 Template.Landing_Section_2.onCreated(function landingSection2OnCreated() {
   this.subscribe(Courses.getPublicationName());
   this.subscribe(Interests.getPublicationName());
   this.subscribe(Opportunities.getPublicationName());
+  this.subscribe(CareerGoals.getPublicationName());
 });
 
 Template.Landing_Section_2.helpers({
   coursesCount() {
     const courseCount = Courses.find().count();
-    const lastDigit = courseCount % 10;
-    return courseCount - lastDigit;
+    // const lastDigit = courseCount % 10;
+    // return courseCount - lastDigit;
+    return courseCount;
   },
   interestsCount() {
     const interestCount = Interests.find().count();
-    const lastDigit = interestCount % 10;
-    return interestCount - lastDigit;
+    // const lastDigit = interestCount % 10;
+    // return interestCount - lastDigit;
+    return interestCount;
   },
   opportunitiesCount() {
     const opportunityCount = Opportunities.find().count();
-    const lastDigit = opportunityCount % 10;
-    return opportunityCount - lastDigit;
+    // const lastDigit = opportunityCount % 10;
+    // return opportunityCount - lastDigit;
+    return opportunityCount;
+  },
+  careerGoalsCount() {
+    const count = CareerGoals.find().count();
+    return count;
   },
 });
 
