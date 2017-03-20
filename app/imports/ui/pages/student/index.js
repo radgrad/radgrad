61d3456e6d3ffb38729f8212aed259c5d4577ba2
Amy Takayesu <amytakayesu@gmail.com>
2017-03-19 17:25:35 -1000

    Removed now empty js files, removed no longer used student files, removed template subscriptions ready on student pages

--- app/imports/ui/pages/student/index.js
+++ app/imports/ui/pages/student/index.js
@@ -1,9 +1,3 @@
-import './student-about-ice.html';
-import './student-about-ice.js';
-
-import './student-about-levels.html';
-import './student-about-levels.js';
-
 import './student-degree-planner-page.html';
 import './student-degree-planner-page.js';
 
@@ -23,25 +17,19 @@ import './student-explorer-opportunities-page.html';
 import './student-explorer-opportunities-page.js';
 
 import './student-explorer-page.html';
-import './student-explorer-page.js';
 
 import './student-explorer-users-page.html';
 import './student-explorer-users-page.js';
 
 import './student-home-aboutme-page.html';
-import './student-home-aboutme-page.js';
 
 import './student-home-ice-page.html';
-import './student-home-ice-page.js';
 
 import './student-home-levels-page.html';
-import './student-home-levels-page.js';
 
 import './student-home-log-page.html';
-import './student-home-log-page.js';
 
 import './student-home-page.html';
-import './student-home-page.js';
 import './student-home-page.css';
 
 import './student-mentorspace-page.css';
