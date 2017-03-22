736e4f71a5ed204a66fd7a3a692b82b3f2664211
Cam Moore <cammymoore07@gmail.com>
2017-03-21 17:06:31 -1000

    Added logging to server

--- app/imports/startup/server/index.js
+++ app/imports/startup/server/index.js
@@ -1,6 +1,7 @@
 import './accounts.js';
 import './check-integrity';
 import './fixtures.js';
+import './logging';
 import './publications.js';
 
 // icsdata/ directory is not imported here intentionally.
---
+++ app/imports/startup/server/logging.js
@@ -0,0 +1,4 @@
+import { Logger } from 'meteor/jag:pince';
+
+// Sets the client logging level values are 'all', 'trace', 'debug', 'info', 'warn', 'error'
+Logger.setLevel('trace');
