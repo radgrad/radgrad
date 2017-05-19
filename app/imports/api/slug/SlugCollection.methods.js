c60e5b1cc8eb19f51bf00fe7500c765825af31b2
Cam Moore <cammymoore07@gmail.com>
2017-05-19 10:12:16 -1000

    Slugs

---
+++ app/imports/api/slug/SlugCollection.methods.js
@@ -0,0 +1,54 @@
+import { Meteor } from 'meteor/meteor';
+import { check } from 'meteor/check';
+import { SimpleSchema } from 'meteor/aldeed:simple-schema';
+import { ValidatedMethod } from 'meteor/mdg:validated-method';
+import { Slugs } from './SlugCollection';
+
+/* eslint object-shorthand: "off" */
+
+/**
+ * Name of the Slug collection define method.
+ * @type {string}
+ */
+export const defineSlugMethodName = 'Slugs.define';
+
+/**
+ * The Slug collection define validated method.
+ */
+export const defineSlugMethod = new ValidatedMethod({
+  name: defineSlugMethodName,
+  validate: new SimpleSchema({
+    name: { type: String, optional: false },
+    entityName: { type: String, optional: false },
+  }).validator(),
+  run({ name, entityName }) {
+    Slugs.define(name, entityName);
+  },
+});
+
+/**
+ * The Slug collection remove method name.
+ * @type {string}
+ */
+export const removeSlugMethodName = 'Slugs.remove';
+
+/**
+ * The Slug collection define validated method.
+ */
+export const removeSlugMethod = new ValidatedMethod({
+  name: removeSlugMethodName,
+  validate: new SimpleSchema({
+    name: { type: String },
+    entityName: { type: String },
+    entityID: { type: SimpleSchema.RegEx.Id, optional: true },
+  }).validator(),
+  run(slug) {
+    Slugs.removeIt(slug);
+  },
+});
+
+// Meteor.methods({
+//   'SlugCollection._removeAll'() {
+//     Slugs._removeAll();
+//   },
+// });
