7818158f8ac23bb1c0d5a1b4b9bfcb3cc7302464
Philip Johnson <johnson@hawaii.edu>
2017-05-30 15:00:26 -1000

    Minor cleanup on PublicStats and Role.

--- app/imports/api/role/Role.js
+++ app/imports/api/role/Role.js
@@ -1,10 +1,11 @@
+import { Roles } from 'meteor/alanning:roles';
+import { _ } from 'meteor/erasaur:meteor-lodash';
+import { Meteor } from 'meteor/meteor';
+
 /** @module api/role/Role */
 
 /** Defines the legal strings used to represent roles in the system. */
 
-import { Roles } from 'meteor/alanning:roles';
-import { _ } from 'meteor/erasaur:meteor-lodash';
-import { Meteor } from 'meteor/meteor';
 
 /**
  * ROLE Provides ROLE.FACULTY, ROLE.STUDENT, ROLE.ADMIN, ROLE.ALUMNI.
