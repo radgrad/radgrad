8c9749139d28abf409c060836ad0ee63c5dd24b9
Cam Moore <cammymoore07@gmail.com>
2017-06-26 08:06:05 -1000

    Checkpoint

--- app/imports/api/mentor/MentorQuestionAndAnswerCollection.methods.app-test.js
+++ app/imports/api/mentor/MentorQuestionAndAnswerCollection.methods.app-test.js
@@ -1,44 +1,78 @@
 import { Meteor } from 'meteor/meteor';
 import { resetDatabaseMethod, defineMethod, removeItMethod, updateMethod } from '../base/BaseCollection.methods';
 import { MentorAnswers } from './MentorAnswerCollection';
+import { MentorQuestions } from './MentorQuestionCollection';
 import { defineTestFixturesMethod, withRadGradSubscriptions, withLoggedInUser } from '../test/test-utilities';
 
 /* eslint prefer-arrow-callback: "off", no-unused-expressions: "off" */
 /* eslint-env mocha */
 
 if (Meteor.isClient) {
-  describe('MentorAnswerCollection Meteor Methods', function test() {
-    const collectionName = MentorAnswers.getCollectionName();
-    const definitionData = {
+  describe('MentorQuestionAndAnswerCollection Meteor Methods', function test() {
+    const answerCollectionName = MentorAnswers.getCollectionName();
+    const questionCollectionName = MentorQuestions.getCollectionName();
+    const answerDefinition = {
 
+    };
+    const questionDefinition = {
+      question: 'question',
+      slug: 'test-question',
+      student: 'abi',
+      moderated: false,
+      visible: false,
+      moderatorComments: '',
     };
 
     before(function (done) {
       this.timeout(0);
-      defineTestFixturesMethod.call(['minimal', 'admin.user'], done);
+      defineTestFixturesMethod.call(['minimal', 'admin.user', 'abi.user'], done);
     });
 
     after(function (done) {
       resetDatabaseMethod.call(null, done);
     });
 
-    it('Define Method', function (done) {
+    it('Question Define Method', function (done) {
       withLoggedInUser().then(() => {
         withRadGradSubscriptions().then(() => {
-          defineMethod.call({ collectionName, definitionData }, done);
+          defineMethod.call({ collectionName: questionCollectionName, definitionData: questionDefinition }, done);
         }).catch(done);
       });
     });
 
-    it('Update Method', function (done) {
+    it('Answer Define Method', function (done) {
       withLoggedInUser().then(() => {
         withRadGradSubscriptions().then(() => {
-          const id = MentorAnswers.findIdBySlug(definitionData.slug);
+          defineMethod.call({ collectionName: answerCollectionName, definitionData: answerDefinition }, done);
+        }).catch(done);
+      });
+    });
+
+    it('Question Update Method', function (done) {
+      withLoggedInUser().then(() => {
+        withRadGradSubscriptions().then(() => {
+          const id = MentorQuestions.findIdBySlug(questionDefinition.slug);
+          const question = 'updated CareerGoal name';
+          const student = 'abi';
+          const moderated = true;
+          const visible = false;
+          const moderatorComments = 'comments';
+          updateMethod.call({ collectionName: answerCollectionName,
+            updateData: { id, question, student, moderated, visible, moderatorComments } }, done);
+        }).catch(done);
+      });
+    });
+
+    it('Answer Update Method', function (done) {
+      withLoggedInUser().then(() => {
+        withRadGradSubscriptions().then(() => {
+          const id = MentorAnswers.findIdBySlug(answerDefinition.slug);
           const name = 'updated CareerGoal name';
           const description = 'updated CareerGoal description';
           const interests = ['algorithms', 'java'];
           const prerequisites = ['ics_111', 'ics_141'];
-          updateMethod.call({ collectionName, updateData: { id, name, description, interests, prerequisites } }, done);
+          updateMethod.call({ collectionName: answerCollectionName,
+            updateData: { id, name, description, interests, prerequisites } }, done);
         }).catch(done);
       });
     });
@@ -46,7 +80,7 @@ if (Meteor.isClient) {
     it('Remove Method', function (done) {
       withLoggedInUser().then(() => {
         withRadGradSubscriptions().then(() => {
-          removeItMethod.call({ collectionName, instance: definitionData.slug }, done);
+          removeItMethod.call({ answerCollectionName, instance: answerDefinition.slug }, done);
         }).catch(done);
       });
     });
