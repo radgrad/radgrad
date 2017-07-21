d73fa9f8044250875acfe10a4b52f81b6e0c0b56
Philip Johnson <johnson@hawaii.edu>
2017-07-20 14:28:51 -1000

    Checkpoint

--- app/imports/ui/components/advisor/student-selector-widget.js
+++ app/imports/ui/components/advisor/student-selector-widget.js
@@ -1,15 +1,15 @@
-import { ReactiveDict } from 'meteor/reactive-dict';
-import { Template } from 'meteor/templating';
-
-// /** @module ui/components/advisor/Student_Selector_Widget */
-
-Template.Student_Selector_Widget.onCreated(function studentSelectorOnCreated() {
-  if (this.data.dictionary) {
-    this.state = this.data.dictionary;
-  } else {
-    this.state = new ReactiveDict();
-  }
-  if (this.data.studentID) {
-    this.studentID = this.data.studentID;
-  }
-});
+// import { ReactiveDict } from 'meteor/reactive-dict';
+// import { Template } from 'meteor/templating';
+//
+// // /** @module ui/components/advisor/Student_Selector_Widget */
+//
+// Template.Student_Selector_Widget.onCreated(function studentSelectorOnCreated() {
+//   if (this.data.dictionary) {
+//     this.state = this.data.dictionary;
+//   } else {
+//     this.state = new ReactiveDict();
+//   }
+//   if (this.data.studentID) {
+//     this.studentID = this.data.studentID;
+//   }
+// });
