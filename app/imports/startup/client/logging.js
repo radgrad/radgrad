008cfe17e5a2bdf95111c7e471d12d8a9979720a
Cam Moore <cammymoore07@gmail.com>
2017-09-29 09:35:53 -1000

    Adding packages

--- app/imports/startup/client/logging.js
+++ app/imports/startup/client/logging.js
@@ -1,4 +1,22 @@
+import { Meteor } from 'meteor/meteor';
 import { Logger } from 'meteor/jag:pince';
+import {
+  ClientLogger, NullSender, ConsoleSender, MeteorClientHttpSender, LeveledStrategy, BrowserProcessor, RoutingProcessor,
+  MeteorUserProcessor,
+} from 'filog';
+
 
 // Sets the client logging level values are 'all', 'trace', 'debug', 'info', 'warn', 'error'
 Logger.setLevel('trace');
+
+export const logger = new ClientLogger(new LeveledStrategy(
+    new NullSender(),
+    new ConsoleSender(),
+    new MeteorClientHttpSender(Meteor.absoluteUrl('logger')),
+));
+
+logger.processors.push(
+    new BrowserProcessor(),
+    new RoutingProcessor(),
+    new MeteorUserProcessor(Meteor),
+);
--- app/imports/startup/server/logging.js
+++ app/imports/startup/server/logging.js
@@ -1,4 +1,12 @@
+import { Mongo } from 'meteor/mongo';
+import { WebApp } from 'meteor/webapp';
 import { Logger } from 'meteor/jag:pince';
-
+import { MongodbSender, ServerLogger, LeveledStrategy, MeteorUserProcessor } from 'filog';
 // Sets the client logging level values are 'all', 'trace', 'debug', 'info', 'warn', 'error'
 Logger.setLevel('trace');
+
+const sender = new MongodbSender(Mongo, 'logger');
+export const logger = new ServerLogger(
+    new LeveledStrategy(sender, sender, sender),
+    WebApp);
+logger.processors.push(new MeteorUserProcessor());
