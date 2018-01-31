3157a4eec9027e067752601b7e48ac680362c566
Cam Moore <cammymoore07@gmail.com>
2018-01-31 10:12:17 -1000

    Corrected the number of pending verification requests

--- app/imports/ui/layouts/faculty/faculty-layout.js
+++ app/imports/ui/layouts/faculty/faculty-layout.js
@@ -1,11 +1,20 @@
 import { Template } from 'meteor/templating';
+import { _ } from 'meteor/erasaur:meteor-lodash';
+import { Opportunities } from '../../../api/opportunity/OpportunityCollection';
+import { OpportunityInstances } from '../../../api/opportunity/OpportunityInstanceCollection';
 import * as RouteNames from '../../../startup/client/router.js';
 import { VerificationRequests } from '../../../api/verification/VerificationRequestCollection';
+import { getUserIdFromRoute } from '../../components/shared/get-user-id-from-route';
 
 Template.Faculty_Layout.helpers({
   secondMenuItems() {
-    let numRequests = 0;
-    numRequests += VerificationRequests.find({ status: 'Open' }).fetch().length;
+    let openRequests = VerificationRequests.find({ status: VerificationRequests.OPEN }).fetch();
+    openRequests = _.filter(openRequests, (request) => {
+      const oi = OpportunityInstances.findDoc(request.opportunityInstanceID);
+      return Opportunities.findDoc(oi.opportunityID).sponsorID === getUserIdFromRoute();
+    });
+
+    const numRequests = openRequests.length;
     let requestsLabel = 'Verification';
     if (numRequests > 0) {
       requestsLabel = `${requestsLabel} (${numRequests})`;
