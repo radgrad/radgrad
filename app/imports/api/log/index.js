5f0dfe3f2021d36a0fff81c631640e30ddf52fc3
Danny Weng <danny.gl.weng@gmail.com>
2018-03-05 12:42:43 -1000

    Added UserInteractionCollection to publishments and subscriptions

--- app/imports/api/log/index.js
+++ app/imports/api/log/index.js
@@ -3,3 +3,6 @@ import './AdvisorLogCollection.js';
 import './AdvisorLogCollection.methods';
 
 import './AppLogCollection';
+
+import './UserInteractionCollection';
+import './UserInteractionCollection.methods';
--- app/imports/ui/layouts/shared/with-global-subscriptions.js
+++ app/imports/ui/layouts/shared/with-global-subscriptions.js
@@ -18,6 +18,7 @@ import { Semesters } from '../../../api/semester/SemesterCollection';
 import { StudentProfiles } from '../../../api/user/StudentProfileCollection';
 import { Slugs } from '../../../api/slug/SlugCollection';
 import { Teasers } from '../../../api/teaser/TeaserCollection';
+import { UserInteractions } from '../../../api/log/UserInteractionCollection';
 import { Users } from '../../../api/user/UserCollection';
 
 /* eslint-disable object-shorthand */
@@ -55,6 +56,7 @@ Template.With_Global_Subscriptions.onCreated(function onCreated() {
     globalSubs.subscribe(Slugs.getPublicationName());
     globalSubs.subscribe(Teasers.getPublicationName());
     globalSubs.subscribe(Users.getPublicationName());
+    globalSubs.subscribe(UserInteractions.getPublicationName());
     self.ready.set(globalSubs.ready());
   });
 });
