a02a10caf1767c33b4d6d37f27a65c4f66f71a35
Philip Johnson <johnson@hawaii.edu>
2018-03-08 15:18:12 -1000

    update bulk data download. Do npm install to get new package.

--- scripts/download-bulk-star-json.js
+++ scripts/download-bulk-star-json.js
@@ -2,15 +2,24 @@ const request = require('request');
 const fs = require('fs');
 const inquirer = require('inquirer');
 const _ = require('lodash');
+const moment = require('moment');
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
+function getStarArrivalTime() {
+  const emailData = fs.readFileSync('./emails.txt');
+  const numEmails = emailData.toString().split('\n').length;
+  const arrivalTime = moment().add(numEmails * 10, 's');
+  // eslint-disable-next-line
+  return `Obtaining STAR data for ${numEmails} users. Results expected at ${arrivalTime.format('h:mm A')} (${arrivalTime.fromNow()})`;
+}
 
 /**
  * Read emails.txt which is a newline delimited list of UH email addresses
  * Pass email list as part of the url encoded form to the STAR-RadGrad api
  * Return the response which should be a JSON object
  */
-function testRadgradCourses(username, password) {
+function getCourseData(username, password) {
   const params = {
     form: {
       uid: username,
@@ -28,6 +37,11 @@ function testRadgradCourses(username, password) {
   });
 }
 
+/**
+ * Determine if a student's data represents an alumni.
+ * This is heuristically determined by checking to see if there are no ICS or EE courses in their course data.
+ * If an alumni is detected, then put them into the alumni.txt file and remove their data from the json file.
+ */
 function filterAlumni(contents) {
   const re = /ics|ee/i;
   const alumniEmail = [];
@@ -44,7 +58,7 @@ function filterAlumni(contents) {
     if (alumni) {
       alumniEmail.push(student.email);
     }
-    console.log(`${student.email} is alumni = ${alumni}.`);
+    // console.log(`${student.email} is alumni = ${alumni}.`);
   });
   const filtered = _.filter(data, function (d) {
     let student = false;
@@ -64,50 +78,41 @@ function filterAlumni(contents) {
   return JSON.stringify(filtered, null, ' ');
 }
 
+/**
+ * Main function. Ask for the admin's username, password, and file to put the json data into.
+ * Then call getCourseData to get the data from STAR.
+ * @returns {Promise<void>}
+ */
 async function downloadStarData() {
   const questions = [
     {
       name: 'username',
       type: 'input',
       message: 'Enter your UH username:',
-      validate: function (value) {
-        if (value.length) {
-          return true;
-        }
-        return 'Please enter your UH username';
-      },
+      validate: value => (value.length ? true : 'Please enter your UH username'),
     },
     {
       name: 'password',
       type: 'password',
       message: 'Enter your password:',
-      validate: function (value) {
-        if (value.length) {
-          return true;
-        }
-        return 'Please enter your password';
-      },
+      validate: value => (value.length ? true : 'Please enter your password'),
     },
     {
       name: 'filename',
       type: 'input',
-      message: 'Enter the file name to save the data to',
-      validate: function (value) {
-        if (value.length) {
-          return true;
-        }
-        return 'Please enter a file name.';
-      },
+      message: 'Enter the file name where the data will be saved:',
+      validate: value => (value.length ? true : 'Please enter the file name.'),
     },
   ];
 
-  const r = await inquirer.prompt(questions);
-  testRadgradCourses(r.username, r.password)
-      .then(res => fs.writeFileSync(r.filename, filterAlumni(res.body)))
+  const userParams = await inquirer.prompt(questions);
+  console.log(getStarArrivalTime());
+  getCourseData(userParams.username, userParams.password)
+      .then(res => fs.writeFileSync(userParams.filename, filterAlumni(res.body)))
       .catch(err => console.log(err));
 }
 
 /**
- * Edit the file emails.txt to list the student email addresses. Then run node download-bulk-star-json.js
+ * emails.txt must have student email addresses, one per line. Then run node download-bulk-star-json.js
  */
 downloadStarData();
