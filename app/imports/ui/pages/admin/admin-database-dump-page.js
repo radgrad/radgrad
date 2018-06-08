5801051412331935ff856041d261081d005616a6
Cam Moore <cammymoore07@gmail.com>
2018-06-07 14:48:46 -1000

    Checkpoint

--- app/imports/ui/pages/admin/admin-database-dump-page.js
+++ app/imports/ui/pages/admin/admin-database-dump-page.js
@@ -4,6 +4,7 @@ import { moment } from 'meteor/momentjs:moment';
 import { ZipZap } from 'meteor/udondan:zipzap';
 import { _ } from 'meteor/erasaur:meteor-lodash';
 import { dumpDatabaseMethod } from '../../../api/base/BaseCollection.methods.js';
+import { generateStudentEmailsMethod } from '../../../api/user/UserCollection.methods';
 
 Template.Admin_DataBase_Dump_Page.helpers({
   errorMessage() {
@@ -13,8 +14,16 @@ Template.Admin_DataBase_Dump_Page.helpers({
     const data = Template.instance().results.get();
     return (data) ? '' : 'hidden';
   },
+  haveResults() {
+    console.log(!_.isNil(Template.instance().results.get()));
+    return !_.isNil(Template.instance().results.get());
+  },
   results() {
     return Template.instance().results.get() || '';
+  },
+  students() {
+    console.log('students', Template.instance().students.get());
+    return Template.instance().students.get() || '';
   },
   successOrError() {
     return Template.instance().successOrError.get();
@@ -31,6 +40,7 @@ Template.Admin_DataBase_Dump_Page.helpers({
 
 Template.Admin_DataBase_Dump_Page.onCreated(function onCreated() {
   this.results = new ReactiveVar();
+  this.students = new ReactiveVar();
   this.successOrError = new ReactiveVar();
   this.timestamp = new ReactiveVar();
 });
@@ -58,4 +68,27 @@ Template.Admin_DataBase_Dump_Page.events({
       }
     });
   },
+  'click .jsStudentEmails': function clickStudentEmails(event, instance) {
+    event.preventDefault();
+    generateStudentEmailsMethod.call(null, (error, result) => {
+      if (error) {
+        console.log('Error during Generating Student Emails: ', error);
+        instance.results.set(error);
+        instance.successOrError.set('error');
+      } else {
+        const data = {};
+        data.name = 'Students';
+        data.contents = result.students;
+        instance.results.set([data]);
+        instance.timestamp.set(new Date());
+        instance.successOrError.set('success');
+        const zip = new ZipZap();
+        const now = moment().format(databaseFileDateFormat);
+        const dir = `radgrad-students${now}`;
+        const fileName = `${dir}/Students.txt`;
+        zip.file(fileName, result.students.join('\n'));
+        zip.saveAs(`${dir}.zip`);
+      }
+    })
+  }
 });
