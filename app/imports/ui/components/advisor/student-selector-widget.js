60c441d2f94492a4ddb1023271f951f844983f37
Cam Moore <cammymoore07@gmail.com>
2018-09-07 12:50:29 -1000

    Checkpoint
    Need to change to a filter so we can filter students by first name, last name, user name.

--- app/imports/ui/components/advisor/student-selector-widget.js
+++ app/imports/ui/components/advisor/student-selector-widget.js
@@ -1,13 +1,25 @@
 // import { ReactiveDict } from 'meteor/reactive-dict';
-// import { Template } from 'meteor/templating';
+import { Template } from 'meteor/templating';
+import { Users } from '../../../api/user/UserCollection';
 //
-// Template.Student_Selector_Widget.onCreated(function studentSelectorOnCreated() {
-//   if (this.data.dictionary) {
-//     this.state = this.data.dictionary;
-//   } else {
-//     this.state = new ReactiveDict();
-//   }
-//   if (this.data.studentID) {
-//     this.studentID = this.data.studentID;
-//   }
-// });
+Template.Student_Selector_Widget.onCreated(function studentSelectorOnCreated() {
+  if (this.data.dictionary) {
+    this.state = this.data.dictionary;
+  }
+  if (this.data.studentID) {
+    this.studentID = this.data.studentID;
+  }
+});
+
+Template.Student_Selector_Widget.events({
+  'click .jsFindStudent': function (event, instance) {
+    event.preventDefault();
+    const studentName = event.target.parentElement.getElementsByTagName('input')[0].value;
+    const profile = Users.getProfile(studentName);
+    if (profile) {
+      instance.studentID.set(studentName);
+    } else {
+      console.log(`${studentName} is not in RadGrad.`);
+    }
+  },
+});
