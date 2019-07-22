06533ba863f69e883579d63922ef45c53c8a97e1
Cam Moore <cammymoore07@gmail.com>
2019-07-22 13:39:24 -1000

    Update card-explorer-courses-page.js
    Checkpoint

--- app/imports/ui/pages/shared/card-explorer-courses-page.js
+++ app/imports/ui/pages/shared/card-explorer-courses-page.js
@@ -8,51 +8,24 @@ import { getUserIdFromRoute } from '../../components/shared/get-user-id-from-rou
 import { AcademicPlans } from '../../../api/degree-plan/AcademicPlanCollection';
 import { ROLE } from '../../../api/role/Role';
 import { StudentProfiles } from '../../../api/user/StudentProfileCollection';
+import { FavoriteCourses } from '../../../api/favorite/FavoriteCourseCollection';
 
 Template.Card_Explorer_Courses_Page.helpers({
   addedCourses() {
     let addedCourses = [];
-    const allCourses = Courses.findNonRetired({}, { sort: { shortName: 1 } });
-    const userID = getUserIdFromRoute();
-    _.forEach(allCourses, (course) => {
-      const ci = CourseInstances.find({
-        studentID: userID,
-        courseID: course._id,
-      }).fetch();
-      if (ci.length > 0) {
-        if (course.shortName !== 'Non-CS Course') {
-          addedCourses.push({ item: course, count: ci.length });
-        }
-      }
-    });
-    if (Roles.userIsInRole(userID, [ROLE.STUDENT])) {
-      const profile = StudentProfiles.findDoc({ userID });
-      const plan = AcademicPlans.findDoc(profile.academicPlanID);
-      // CAM: why are we filtering?
-      if (plan.coursesPerSemester.length < 15) { // not bachelors and masters
-        const regex = /[1234]\d\d/g;
-        addedCourses = _.filter(addedCourses, (c) => c.item.number.match(regex));
-      }
-    }
-    console.log('addedCourses %o', addedCourses);
-    return addedCourses;
+    const studentID = getUserIdFromRoute();
+    const favorites = FavoriteCourses.find({ studentID }).fetch();
+    return _.map(favorites, (f) => ({ item: Courses.findDoc(f.courseID), count: 1 }));
   },
   nonAddedCourses() {
     const allCourses = Courses.findNonRetired({}, { sort: { shortName: 1 } });
-    const userID = getUserIdFromRoute();
+    const studentID = getUserIdFromRoute();
+    const favoriteIDs = _.map(FavoriteCourses.find({ studentID }).fetch(), (f) => f.courseID);
+
     const nonAddedCourses = _.filter(allCourses, function (course) {
-      const ci = CourseInstances.find({
-        studentID: userID,
-        courseID: course._id,
-      }).fetch();
-      if (ci.length > 0) {
-        return false;
-      }
-      if (course.shortName === 'Non-CS Course') {
-        return false;
-      }
-      return true;
+      return !_.includes(favoriteIDs, course._id);
     });
+    console.log(nonAddedCourses);
     return nonAddedCourses;
   },
 });
