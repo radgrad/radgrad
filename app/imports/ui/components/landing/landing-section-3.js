6e229248aa4c33543c8154f586a0467e8441c125
Cam Moore <cammymoore07@gmail.com>
2019-09-26 08:21:55 -1000

    Removing unused helpers

--- app/imports/ui/components/landing/landing-section-3.js
+++ app/imports/ui/components/landing/landing-section-3.js
@@ -1,24 +1,11 @@
 import { Template } from 'meteor/templating';
 import * as RouteNames from '../../../startup/client/router.js';
-import { Interests } from '../../../api/interest/InterestCollection';
-import { Slugs } from '../../../api/slug/SlugCollection';
 
 Template.Landing_Section_3.helpers({
   careerGoalsRouteName() {
     return RouteNames.landingCardExplorerCareerGoalsPageRouteName;
   },
-  degreesRouteName() {
-    return RouteNames.landingCardExplorerDegreesPageRouteName;
-  },
-  firstInterest() {
-    let ret = 'dotNet';
-    const interests = Interests.find({}, { sort: { name: 1 } }).fetch();
-    if (interests.length > 0) {
-      ret = Slugs.findDoc(interests[0].slugID).name;
-    }
-    return ret;
-  },
   interestsRouteName() {
-    return RouteNames.landingExplorerInterestsPageRouteName;
+    return RouteNames.landingCardExplorerInterestsPageRouteName;
   },
 });
