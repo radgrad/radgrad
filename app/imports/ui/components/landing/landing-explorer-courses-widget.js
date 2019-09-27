e6dd03cdd1bfddaf90e736abb77b565946dda56c
Cam Moore <cammymoore07@gmail.com>
2019-09-27 10:20:01 -1000

    Landing_Explorer_Course_Widget

--- app/imports/ui/components/landing/landing-explorer-courses-widget.js
+++ app/imports/ui/components/landing/landing-explorer-courses-widget.js
@@ -1,93 +1,29 @@
 import { Template } from 'meteor/templating';
-import { _ } from 'meteor/erasaur:meteor-lodash';
 import * as RouteNames from '../../../startup/client/router.js';
 import { Slugs } from '../../../api/slug/SlugCollection.js';
 import { Courses } from '../../../api/course/CourseCollection.js';
-import { Semesters } from '../../../api/semester/SemesterCollection.js';
-import { CourseInstances } from '../../../api/course/CourseInstanceCollection.js';
-import { getUserIdFromRoute } from '../shared/get-user-id-from-route';
 import { isLabel } from '../../utilities/template-helpers';
 import { Teasers } from '../../../api/teaser/TeaserCollection';
 
 Template.Landing_Explorer_Courses_Widget.helpers({
-  color(table) {
-    if (table.length === 0) {
-      return 'whitesmoke';
-    }
-    return '';
-  },
   courseNameFromSlug(courseSlugName) {
-    const slug = Slugs.findDoc({ name: courseSlugName });
-    const course = Courses.findDoc({ slugID: slug._id });
+    // console.log(courseSlugName);
+    const slug = Slugs.findNonRetired({ name: courseSlugName });
+    const course = Courses.findDoc({ slugID: slug[0]._id });
     return course.shortName;
   },
   coursesRouteName() {
     return RouteNames.landingExplorerCoursesPageRouteName;
   },
-  futureInstance(course) {
-    let ret = false;
-    const ci = CourseInstances.find({
-      studentID: getUserIdFromRoute(),
-      courseID: course._id,
-    }).fetch();
-    _.forEach(ci, function (courseInstance) {
-      if (Semesters.findDoc(courseInstance.semesterID).semesterNumber >=
-          Semesters.getCurrentSemesterDoc().semesterNumber) {
-        ret = true;
-      }
-    });
-    return ret;
-  },
-  getTableTitle(tableIndex, table) {
-    switch (tableIndex) {
-      case 0:
-        if (table.length !== 0) {
-          return '<h4><i class="green checkmark icon"></i>Completed</h4>';
-        }
-        return '<h4 style="color:grey"><i class="grey checkmark icon"></i>Completed</h4>';
-      case 1:
-        if (table.length !== 0) {
-          return '<h4><i class="yellow warning sign icon"></i>In Plan (Not Yet Completed)</h4>';
-        }
-        return '<h4 style="color:grey"><i class="grey warning sign icon"></i>In Plan (Not Yet Completed)</h4>';
-      case 2:
-        if (table.length !== 0) {
-          return '<h4><i class="red warning circle icon"></i>Not in Plan</h4>';
-        }
-        return '<h4 style="color:grey"><i class="grey warning circle icon"></i>Not in Plan</h4>';
-      default:
-        return 'ERROR: More than one table.';
-    }
-  },
   hasTeaser(item) {
-    const teaser = Teasers.find({ targetSlugID: item.slugID }).fetch();
+    const teaser = Teasers.findNonRetired({ targetSlugID: item.slugID });
     return teaser.length > 0;
   },
   isLabel,
   length(table) {
     return table.length !== 0;
   },
-  passedCourse(course) {
-    let ret = false;
-    const ci = CourseInstances.find({
-      studentID: getUserIdFromRoute(),
-      courseID: course._id,
-    }).fetch();
-    _.forEach(ci, function (c) {
-      if (c.grade === 'A+' || c.grade === 'A' || c.grade === 'A-' ||
-          c.grade === 'B+' || c.grade === 'B') {
-        ret = true;
-      }
-    });
-    return ret;
-  },
-  review() {
-    return '';
-  },
   toUpper(string) {
     return string.toUpperCase();
-  },
-  userStatus(course) { // eslint-disable-line
-    return false;
   },
 });
--- app/imports/ui/components/shared/explorer-courses-widget.js
+++ app/imports/ui/components/shared/explorer-courses-widget.js
@@ -9,17 +9,12 @@ import { Semesters } from '../../../api/semester/SemesterCollection.js';
 import { CourseInstances } from '../../../api/course/CourseInstanceCollection.js';
 import { getUserIdFromRoute } from './get-user-id-from-route';
 import { isInRole, isLabel } from '../../utilities/template-helpers';
-import { isSingleChoice } from '../../../api/degree-plan/PlanChoiceUtilities';
 import { Teasers } from '../../../api/teaser/TeaserCollection';
 
 Template.Explorer_Courses_Widget.helpers({
-  choices(prerequisite) {
-    // console.log(prerequisite);
-    return prerequisite.course.split(',');
-  },
   courseNameFromSlug(courseSlugName) {
     // console.log(courseSlugName);
-    const slug = Slugs.find({ name: courseSlugName }).fetch();
+    const slug = Slugs.findNonRetired({ name: courseSlugName });
     const course = Courses.findDoc({ slugID: slug[0]._id });
     return course.shortName;
   },
@@ -82,34 +77,13 @@ Template.Explorer_Courses_Widget.helpers({
     }
   },
   hasTeaser(item) {
-    const teaser = Teasers.find({ targetSlugID: item.slugID }).fetch();
+    const teaser = Teasers.findNonRetired({ targetSlugID: item.slugID });
     return teaser.length > 0;
   },
   isInRole,
   isLabel,
   length(table) {
     return table.length !== 0;
-  },
-  isSingleChoice(prerequisite) {
-    // console.log(prerequisite);
-    return isSingleChoice(prerequisite.course);
-  },
-  isFirst(index) {
-    return index === 0;
-  },
-  passedCourse(course) {
-    let ret = false;
-    const ci = CourseInstances.find({
-      studentID: getUserIdFromRoute(),
-      courseID: course._id,
-    }).fetch();
-    _.forEach(ci, function (c) {
-      if (c.grade === 'A+' || c.grade === 'A' || c.grade === 'A-' ||
-          c.grade === 'B+' || c.grade === 'B') {
-        ret = true;
-      }
-    });
-    return ret;
   },
   review() {
     let review = '';
