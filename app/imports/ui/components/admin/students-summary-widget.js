f7b8b7e303af97cec617f5a3c04fb3789c264413
Cam Moore <cammymoore07@gmail.com>
2020-02-27 09:16:19 -1000

    Checkpoint

--- app/imports/ui/components/admin/students-summary-widget.js
+++ app/imports/ui/components/admin/students-summary-widget.js
@@ -16,82 +16,118 @@ Template.Students_Summary_Widget.onCreated(function studentsSummaryWidgetOnRende
   this.working = new ReactiveVar(false);
   this.unique = new ReactiveVar(0);
   this.enrolled = new ReactiveVar(0);
+  this.totalSessions = new ReactiveVar(0);
   /* There is a possible bug with Semantic where the timeline modal is duplicated in the DOM
     upon navigating back to the student summary page from any other page. A suggested fix (temporary)
     is to simply remove the timeline modal every time the template is created again. */
-  $('#timeline').remove();
+  $('#timeline')
+    .remove();
 });
 
 Template.Students_Summary_Widget.helpers({
   behaviors() {
-    return Template.instance().behaviors.get();
+    return Template.instance()
+      .behaviors
+      .get();
   },
   dateRange() {
-    const dateRange = Template.instance().dateRange.get();
+    const dateRange = Template.instance()
+      .dateRange
+      .get();
     if (_.isEmpty(dateRange)) {
       return '';
     }
-    const startDate = moment(dateRange.startDate).format('MM-DD-YYYY');
-    const endDate = moment(dateRange.endDate).format('MM-DD-YYYY');
+    const startDate = moment(dateRange.startDate)
+      .format('MM-DD-YYYY');
+    const endDate = moment(dateRange.endDate)
+      .format('MM-DD-YYYY');
     return `${startDate} to ${endDate}`;
   },
   uniqueVisitors() {
-    const uniqueVisitors = Template.instance().unique.get();
+    const uniqueVisitors = Template.instance()
+      .unique
+      .get();
     if (uniqueVisitors > 0) {
-      return `Unique Visitors: ${uniqueVisitors}`;
+      return `${uniqueVisitors} Unique Visitors,`;
     }
     return '';
   },
   enrolledStudents() {
-    const enrolledStudents = Template.instance().enrolled.get();
+    const enrolledStudents = Template.instance()
+      .enrolled
+      .get();
     if (enrolledStudents > 0) {
-      return `Enrolled Students: ${enrolledStudents}`;
+      return `${enrolledStudents} Enrolled Students`;
+    }
+    return '';
+  },
+  totalSessions() {
+    const totalSessions = Template.instance()
+      .totalSessions
+      .get();
+    if (totalSessions > 0) {
+      return `${totalSessions} Sessions,`;
     }
     return '';
   },
   percent(count) {
-    const enrolledStudents = Template.instance().enrolled.get();
+    const enrolledStudents = Template.instance()
+      .enrolled
+      .get();
     return ((count / enrolledStudents) * 100).toFixed(0);
   },
   selectedUser() {
-    return !_.isEmpty(Template.instance().selectedUser.get());
+    return !_.isEmpty(Template.instance()
+      .selectedUser
+      .get());
   },
   renderedSummary() {
-    return !_.isEmpty(Template.instance().dateRange.get());
+    return !_.isEmpty(Template.instance()
+      .dateRange
+      .get());
   },
   timelineChart() {
-    const interactionsByUser = Template.instance().interactionsByUser.get();
-    const enrolledStudents = Template.instance().enrolled.get();
+    const interactionsByUser = Template.instance()
+      .interactionsByUser
+      .get();
+    const enrolledStudents = Template.instance()
+      .enrolled
+      .get();
     if (interactionsByUser) {
-      const dateRange = Template.instance().dateRange.get();
+      const dateRange = Template.instance()
+        .dateRange
+        .get();
       const startDate = moment(dateRange.startDate, 'MMMM D, YYYY');
       const endDate = moment(dateRange.endDate, 'MMMM D, YYYY');
       const numDays = endDate.diff(startDate, 'days') + 1;
       const behaviorsByDate = {};
       _.times(numDays, function (index) {
-        const date = moment(startDate).add(index, 'days');
-        behaviorsByDate[moment(date).format('MMM D, YYYY')] = [];
+        const date = moment(startDate)
+          .add(index, 'days');
+        behaviorsByDate[moment(date)
+          .format('MMM D, YYYY')] = [];
       });
       const behaviorList = ['Log In', 'Change Outlook', 'Exploration', 'Planning', 'Verification',
         'Reviewing', 'Mentorship', 'Level Up', 'Complete Plan', 'Profile', 'Log Out'];
       _.each(behaviorsByDate, function (array, date, obj) {
         _.each(interactionsByUser, function (interactions) {
           const interactionsWithinDate = _.filter(interactions, function (interaction) {
-            const interactionDate = moment(interaction.timestamp).format('MMM D, YYYY');
+            const interactionDate = moment(interaction.timestamp)
+              .format('MMM D, YYYY');
             return interactionDate === date;
           });
           if (_.some(interactionsWithinDate, { type: 'login' })) {
             obj[date].push(behaviorList[0]);
           }
           if (_.some(interactionsWithinDate, (i) => i.type === 'careerGoalIDs' || i.type === 'interestIDs'
-              || i.type === 'academicPlanID')) {
+            || i.type === 'academicPlanID')) {
             obj[date].push(behaviorList[1]);
           }
           if (_.some(interactionsWithinDate, (i) => i.type === 'pageView' && i.typeData[0].includes('explorer/'))) {
             obj[date].push(behaviorList[2]);
           }
           if (_.some(interactionsWithinDate, (i) => i.type === 'addCourse' || i.type === 'removeCourse'
-              || i.type === 'addOpportunity' || i.type === 'removeOpportunity')) {
+            || i.type === 'addOpportunity' || i.type === 'removeOpportunity')) {
             obj[date].push(behaviorList[3]);
           }
           if (_.some(interactionsWithinDate, (i) => i.type === 'verifyRequest')) {
@@ -101,7 +137,7 @@ Template.Students_Summary_Widget.helpers({
             obj[date].push(behaviorList[5]);
           }
           if (_.some(interactionsWithinDate, (i) => (i.type === 'pageView'
-              && i.typeData[0].includes('mentor-space')) || i.type === 'askQuestion')) {
+            && i.typeData[0].includes('mentor-space')) || i.type === 'askQuestion')) {
             obj[date].push(behaviorList[6]);
           }
           if (_.some(interactionsWithinDate, (i) => i.type === 'level')) {
@@ -183,10 +219,14 @@ Template.Students_Summary_Widget.helpers({
     return { title: { text: null } };
   },
   user() {
-    return Template.instance().selectedUser.get();
+    return Template.instance()
+      .selectedUser
+      .get();
   },
   working() {
-    return Template.instance().working.get();
+    return Template.instance()
+      .working
+      .get();
   },
 });
 
@@ -194,18 +234,23 @@ Template.Students_Summary_Widget.events({
   submit(event, instance) {
     event.preventDefault();
     instance.working.set(true);
-    const startDate = moment(event.target.startDate.value, 'MMMM D, YYYY').toDate();
-    const endDate = moment(event.target.endDate.value, 'MMMM D, YYYY').endOf('day').toDate();
+    const startDate = moment(event.target.startDate.value, 'MMMM D, YYYY')
+      .toDate();
+    const endDate = moment(event.target.endDate.value, 'MMMM D, YYYY')
+      .endOf('day')
+      .toDate();
     const dateRange = { startDate, endDate };
     const startTerm = Semesters.getSemesterDoc(startDate);
     const endTerm = Semesters.getSemesterDoc(endDate);
-    let courseInstances;
-    if (startTerm.semesterNumber !== endTerm.semesterNumber) {
-      courseInstances = CourseInstances.find({ semesterID: startTerm._id }).fetch();
-      courseInstances = courseInstances.concat(CourseInstances.find({ semesterID: endTerm._id }).fetch());
-    } else {
-      courseInstances = CourseInstances.find({ semesterID: startTerm._id }).fetch();
-    }
+    const semesters = Semesters.find({
+      semesterNumber: { $gte: startTerm.semesterNumber, $lte: endTerm.semesterNumber },
+    })
+      .fetch();
+    let courseInstances = [];
+    _.forEach(semesters, (s) => {
+      courseInstances = courseInstances.concat(CourseInstances.find({ semesterID: s._id })
+        .fetch());
+    });
     const enrolledStudents = _.uniq(_.map(courseInstances, (ci) => ci.studentID)).length;
     // console.log(enrolledStudents);
     instance.enrolled.set(enrolledStudents);
@@ -219,96 +264,106 @@ Template.Students_Summary_Widget.events({
         const uniqueVisitors = Object.keys(users).length;
         // console.log(users, uniqueVisitors);
         instance.unique.set(uniqueVisitors);
-        const behaviors = [{
-          type: 'Log In', count: 0, users: [], description: 'Logged into application',
-        },
-        {
-          type: 'Change Outlook', count: 0, users: [], description: 'Updated interests, career goals, or degree',
-        },
-        {
-          type: 'Exploration', count: 0, users: [], description: 'Viewed entries in Explorer',
-        },
-        {
-          type: 'Planning', count: 0, users: [], description: 'Added or removed course/opportunity',
-        },
-        {
-          type: 'Verification', count: 0, users: [], description: 'Requested verification',
-        },
-        {
-          type: 'Reviewing', count: 0, users: [], description: 'Reviewed a course',
-        },
-        {
-          type: 'Mentorship', count: 0, users: [], description: 'Visited the MentorSpace page or asked a question',
-        },
-        {
-          type: 'Level Up', count: 0, users: [], description: 'Leveled up',
-        },
-        {
-          type: 'Complete Plan', count: 0, users: [], description: 'Created a plan with 100 ICE',
-        },
-        {
-          type: 'Profile', count: 0, users: [], description: 'Updated personal image or website url',
-        },
-        {
-          type: 'Log Out', count: 0, users: [], description: 'Logged out',
-        }];
+        const behaviors = [
+          {
+            type: 'Change Outlook', count: 0, users: [], description: 'Updated interests, career goals, or degree',
+          },
+          {
+            type: 'Exploration', count: 0, users: [], description: 'Viewed entries in Explorer',
+          },
+          {
+            type: 'Planning', count: 0, users: [], description: 'Added or removed course/opportunity',
+          },
+          {
+            type: 'Verification', count: 0, users: [], description: 'Requested verification',
+          },
+          {
+            type: 'Reviewing', count: 0, users: [], description: 'Reviewed a course',
+          },
+          {
+            type: 'Mentorship', count: 0, users: [], description: 'Visited the MentorSpace page or asked a question',
+          },
+          {
+            type: 'Level Up', count: 0, users: [], description: 'Leveled up',
+          },
+          {
+            type: 'Complete Plan', count: 0, users: [], description: 'Created a plan with 100 ICE',
+          },
+          {
+            type: 'Profile', count: 0, users: [], description: 'Updated personal image or website url',
+          },
+          {
+            type: 'Log Out', count: 0, users: [], description: 'Logged out',
+          }];
+        let sessions = 0;
         _.each(users, function (interactions, user) {
-          if (_.some(interactions, { type: 'login' })) {
-            behaviors[0].count++;
-            behaviors[0].users.push(user);
-          }
+          let lastTimeStamp;
+          _.forEach(interactions, (i) => {
+            if (_.isUndefined(lastTimeStamp)) {
+              // console.log('init');
+              lastTimeStamp = moment(i.timestamp);
+            } else {
+              // console.log('next');
+              const currentTimestamp = moment(i.timestamp);
+              if (currentTimestamp.diff(lastTimeStamp, 'hours', true) > 1) {
+                sessions++;
+              }
+              lastTimeStamp = currentTimestamp;
+            }
+          });
           if (_.some(interactions, (i) => i.type === 'careerGoalIDs' || i.type === 'interestIDs'
-              || i.type === 'academicPlanID' || (i.type === 'favoriteItem' && i.typeData[0].includes('CareerGoal'))
+            || i.type === 'academicPlanID' || (i.type === 'favoriteItem' && i.typeData[0].includes('CareerGoal'))
             || (i.type === 'unFavoriteItem' && i.typeData[0].includes('CareerGoal'))
             || (i.type === 'favoriteItem' && i.typeData[0].includes('Interest'))
             || (i.type === 'unFavoriteItem' && i.typeData[0].includes('Interest'))
             || (i.type === 'favoriteItem' && i.typeData[0].includes('AcademicPlan'))
             || (i.type === 'unFavoriteItem' && i.typeData[0].includes('AcademicPlan')))) {
+            behaviors[0].count++;
+            behaviors[0].users.push(user);
+          }
+          if (_.some(interactions, (i) => i.type === 'pageView' && i.typeData[0].includes('explorer/'))) {
             behaviors[1].count++;
             behaviors[1].users.push(user);
           }
-          if (_.some(interactions, (i) => i.type === 'pageView' && i.typeData[0].includes('explorer/'))) {
+          if (_.some(interactions, (i) => i.type === 'addCourse' || i.type === 'removeCourse'
+            || i.type === 'addOpportunity' || i.type === 'removeOpportunity')) {
             behaviors[2].count++;
             behaviors[2].users.push(user);
           }
-          if (_.some(interactions, (i) => i.type === 'addCourse' || i.type === 'removeCourse'
-              || i.type === 'addOpportunity' || i.type === 'removeOpportunity')) {
+          if (_.some(interactions, (i) => i.type === 'verifyRequest')) {
             behaviors[3].count++;
             behaviors[3].users.push(user);
           }
-          if (_.some(interactions, (i) => i.type === 'verifyRequest')) {
+          if (_.some(interactions, (i) => i.type === 'addReview')) {
             behaviors[4].count++;
             behaviors[4].users.push(user);
           }
-          if (_.some(interactions, (i) => i.type === 'addReview')) {
-            behaviors[5].count++;
-            behaviors[5].users.push(user);
-          }
           if (_.some(interactions, (i) => (i.type === 'pageView' && i.typeData[0].includes('mentor-space'))
-              || i.type === 'askQuestion')) {
-            behaviors[6].count++;
+            || i.type === 'askQuestion')) {
+            behaviors[5].count++;
             behaviors[6].users.push(user);
           }
           if (_.some(interactions, (i) => i.type === 'level')) {
+            behaviors[6].count++;
+            behaviors[6].users.push(user);
+          }
+          if (_.some(interactions, (i) => i.type === 'completePlan')) {
             behaviors[7].count++;
             behaviors[7].users.push(user);
           }
-          if (_.some(interactions, (i) => i.type === 'completePlan')) {
+          if (_.some(interactions, (i) => i.type === 'picture' || i.type === 'website')) {
             behaviors[8].count++;
             behaviors[8].users.push(user);
           }
-          if (_.some(interactions, (i) => i.type === 'picture' || i.type === 'website')) {
+          if (_.some(interactions, { type: 'logout' })) {
             behaviors[9].count++;
             behaviors[9].users.push(user);
-          }
-          if (_.some(interactions, { type: 'logout' })) {
-            behaviors[10].count++;
-            behaviors[10].users.push(user);
           }
         });
         instance.interactionsByUser.set(users);
         instance.behaviors.set(behaviors);
         instance.dateRange.set(dateRange);
+        instance.totalSessions.set(sessions);
         instance.working.set(false);
       }
     });
@@ -318,29 +373,37 @@ Template.Students_Summary_Widget.events({
     const username = event.target.value;
     const selectedUser = { username, interactions: instance.interactionsByUser.get()[username] };
     instance.selectedUser.set(selectedUser);
-    $('#timeline').modal('show');
+    $('#timeline')
+      .modal('show');
   },
   'click .chart.item': function reflow(event) {
     event.preventDefault();
-    $('#timeline-chart').highcharts().reflow();
+    $('#timeline-chart')
+      .highcharts()
+      .reflow();
   },
 });
 
 Template.Students_Summary_Widget.onRendered(function studentsSummaryWidgetOnRendered() {
-  this.$('#range-start').calendar({
-    type: 'date',
-    endCalendar: this.$('#range-end'),
-  });
-  this.$('#range-end').calendar({
-    type: 'date',
-    startCalendar: this.$('#range-start'),
-  });
-  this.$('.ui.form').form({
-    fields: {
-      startDate: 'empty',
-      endDate: 'empty',
-    },
-  });
-  this.$('.ui.accordion').accordion();
-  this.$('.pointing.menu .item').tab();
+  this.$('#range-start')
+    .calendar({
+      type: 'date',
+      endCalendar: this.$('#range-end'),
+    });
+  this.$('#range-end')
+    .calendar({
+      type: 'date',
+      startCalendar: this.$('#range-start'),
+    });
+  this.$('.ui.form')
+    .form({
+      fields: {
+        startDate: 'empty',
+        endDate: 'empty',
+      },
+    });
+  this.$('.ui.accordion')
+    .accordion();
+  this.$('.pointing.menu .item')
+    .tab();
 });
